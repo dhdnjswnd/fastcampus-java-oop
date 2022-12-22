55f788d52338e28a1ff839a95bedb62606379a49
dhdnjswnd <dhdnjswnd@naver.com>
2022-12-22 22:59:31 +0900

    Update #3 - Main
    qustn zmffotmfmf dlsxjvpdltmfh qusrud
    #3은 여기서 마무리

--- src/main/java/org/fastcampus/Main.java
+++ src/main/java/org/fastcampus/Main.java
@@ -2,13 +2,14 @@ package org.fastcampus;
 
 import org.fastcampus.logic.BubbleSort;
 import org.fastcampus.logic.Sort;
+import org.fastcampus.service.SortService;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Sort<String> sort = new BubbleSort<>();
-        System.out.println("{result} " + sort.sort(Arrays.asList(args)));
+        SortService sortService=new SortService(new BubbleSort<>());
+        System.out.println("{result} " + sortService.doSort(Arrays.asList(args)));
     }
 }
