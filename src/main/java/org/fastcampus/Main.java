ad87e7505a34d75506438f0c688e3894491fe8b4
dhdnjswnd <dhdnjswnd@naver.com>
2022-12-22 22:52:56 +0900

    Design #3 Sort Interface
    인터페이스 설계하고, 각 구현체가 인터페이스를 구현하는 구조로 수정

--- src/main/java/org/fastcampus/Main.java
+++ src/main/java/org/fastcampus/Main.java
@@ -1,13 +1,14 @@
 package org.fastcampus;
 
 import org.fastcampus.logic.BubbleSort;
+import org.fastcampus.logic.Sort;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new BubbleSort<>();
         System.out.println("{result} " + sort.sort(Arrays.asList(args)));
     }
 }
