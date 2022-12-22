6cfbc05862d9194db0ba78c165d5adc92588ad1b
dhdnjswnd <dhdnjswnd@naver.com>
2022-12-22 22:38:23 +0900

    Implement main entry point
    실행될 메인 로직 작성하고 정렬 알고리즘 사용

--- src/main/java/org/fastcampus/Main.java
+++ src/main/java/org/fastcampus/Main.java
@@ -1,7 +1,13 @@
 package org.fastcampus;
 
+import org.fastcampus.logic.BubbleSort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BubbleSort<String> sort = new BubbleSort<>();
+        System.out.println("{result} " + sort.sort(Arrays.asList(args)));
     }
 }
